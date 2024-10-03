d9cb9f289f581f7231107c480813fff7ffa41d6b
dolphin <20delfirio@gmail.com>
2024-10-03 14:20:46 -0600

    Se agregaron los ejemplos de Javascript

---
+++ actividades/a01/src/main.js
@@ -0,0 +1,53 @@
+
+function Ej1(){
+    document.write('Hola mundo');
+}
+function Ej2(){
+    var nombre = window.prompt("Nombre:", "");
+    var edad = prompt("Edad:", "");
+    var altura = prompt("Altura:", "");
+    var casado = prompt("Casado:", "");
+    
+    document.write( nombre );
+    document.write( '<br>' );
+    document.write( edad );
+    document.write( '<br>' );
+    document.write( altura );
+    document.write( '<br>' );
+    document.write( casado );
+}
+function Ej3(){
+    var nombre;
+    var edad;
+    nombre = prompt('Ingresa tu nombre:', '');
+    edad = prompt('Ingresa tu edad:', '');
+    document.write('Hola ');
+    document.write(nombre);
+    document.write(' así que tienes ');
+    document.write(edad);
+    document.write(' años');
+}
+
+function Ej4(){
+    var valor1;
+    var valor2;
+    valor1 = prompt('Introducir primer número:', '');
+    valor2 = prompt('Introducir segundo número', '');
+    var suma = parseInt(valor1)+parseInt(valor2);
+    var producto = parseInt(valor1)*parseInt(valor2);
+    document.write('La suma es ');
+    document.write(suma);
+    document.write('<br>');
+    document.write('El producto es ');
+    document.write(producto);   
+}
+
+function Ej5(){
+    var nombre;
+    var nota;
+    nombre = prompt('Ingresa tu nombre:', '');
+    nota = prompt('Ingresa tu nota:', '');
+    if (nota>=4) {
+    document.write(nombre+' esta aprobado con un '+nota);
+    }    
+}
